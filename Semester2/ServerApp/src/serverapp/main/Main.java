b4aaf861b522e549ae976746f83b81fc2afc54bd
Tomasz Scharmach <scharmach.tomasz@gmail.com>
2015-10-24 17:33:26 +0200

    ServerApp created

---
+++ Semester2/ServerApp/src/serverapp/main/Main.java
@@ -0,0 +1,20 @@
+package serverapp.main;
+/**
+ * @author Tom
+ *
+ * @created on 24 paü 2015
+ *
+ *
+ * Main method for apllication
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Main method");
+
+	}
+
+}
